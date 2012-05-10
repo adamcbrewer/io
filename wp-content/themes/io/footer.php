4ab71d681ee4c4093eef56af9755079546af059f
Adam Brewer <adamcbrewer@gmail.com>
2012-05-10 22:33:27 +0100

    Base styles...done! We have lift-off

--- wp-content/themes/io/footer.php
+++ wp-content/themes/io/footer.php
@@ -11,20 +11,32 @@
 				<br />
 				<br />
 				<p>
-					&copy; <a target="_blank" title="adamcbrewer.com" href="http://adamcbrewer.com">Adam Brewer</a> <?php echo date('Y', time('now')); ?>. I use <a title="@adamcbrewer" target="_blank" href="http://twitter.com/adamcbrewer">Twitter</a> and <a target="_blank" title="adamcbrewer on GitHub" href="https://github.com/adamcbrewer">GitHub</a>
+					&copy; <a target="_blank" title="adamcbrewer.com" href="http://adamcbrewer.com">Adam Brewer</a> <?php echo date('Y', time('now')); ?>. I &#8220;&#9835; &#9834;&#8221; on <a title="@adamcbrewer" target="_blank" href="http://twitter.com/adamcbrewer">Twitter</a>, &#10084; <a target="_blank" title="adamcbrewer on GitHub" href="https://github.com/adamcbrewer">GitHub</a> and <span class="html-char lrg">&#9997;</span> <a title="if all-else fails" href="mailto:adamcbrewer@gmail.com">Gmail</a>
 				</p>
 
 			</footer><!-- footer -->
 
-		<?php
-			/* Always have wp_footer() just before the closing </body>
-			 * tag of your theme, or you will break many plugins, which
-			 * generally use this hook to reference JavaScript files.
-			 */
-			wp_footer();
-		?>
+		<?php wp_footer(); ?>
 
 	</div> <!-- #wrapper -->
+
+	<script type="text/javascript" charset="utf-8">
+		/*
+		 * Init global object (for creating base_url methods, etc.)
+		 */
+		var IO = {
+			basePath: "<?php echo $_SERVER['HTTP_HOST']; ?>"
+		};
+
+		/*
+		 * Add a data-attribute of the user-agent to <html>
+		 *
+		 * Target with: html[data-useragent*="Chrome/13.0"][data-platform="Win32"] ...
+		 */
+		var b = document.documentElement;
+		b.setAttribute("data-useragent",  navigator.userAgent);
+		b.setAttribute("data-platform", navigator.platform );
+	</script>
 
 	<script type="text/javascript">
 
--- wp-content/themes/io/header.php
+++ wp-content/themes/io/header.php
@@ -6,7 +6,23 @@
 <!--[if (gt IE 9)|!(IE)]><!--><html <?php language_attributes(); ?> class="no-js"><!--<![endif]-->
 	
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>" />
+	<meta charset="utf-8">
+
+	<!-- 	
+		Make a DNS handshake with a foreign domain, so the connection 
+		goes faster when the user eventually needs to access it. 
+		This works well for loading in assets (like images) from another domain, 
+		or a JavaScript library from a CDN.
+	-->
+	<link rel="dns-prefetch" href="//ajax.googleapis.com">
+
+	<!-- 
+		IE10 does not support plugins, such as Flash, in Metro Mode. 
+		If your site requires plugins, you can let users know that via the 
+		X-UA-Compatible meta element (requiresActiveX=true), which will prompt them to switch to Desktop Mode.
+	-->
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1,requiresActiveX=true">
+
 	<title>
 		<?php
 			/*
@@ -19,23 +35,51 @@
 		?>
 		{ io: <?php echo $title ? $title : 'null'; ?> }
 	</title>
+
+	<!-- META -->
+	<meta http-equiv="Content-Language" content="en-gb">
+	<meta http-equiv="imagetoolbar" content="false">
+	<meta name="MSSmartTagsPreventParsing" content="true">
+	<meta name="robots" content="all">
+	
+	<meta name="Author" content="">
+	<link type="text/plain" rel="author" href="humans.txt" />
+	<meta name="Copyright" content="">
+	
+	<meta name="description" content="">
+	<meta name="keywords" content="">
+
+	<!-- FACEBOOK OPEN GRAPH -->
+	<meta property="og:title" content=""/> <!-- subject title -->
+	<meta property="og:type" content=""/> <!-- More information: http://developers.facebook.com/docs/opengraph/ -->
+	<meta property="og:image" content=""/> <!-- image must be at least 50px by 50px with a maximum ratio of 3:1 -->
+	<meta property="og:url" content=""/> <!-- URL of the page being liked -->
+	<meta property="og:site_name" content=""/> <!-- A human-readable name for your site -->
+	<meta property="og:description" content=""/> <!-- basic site description -->
+
+	<!-- 
+		Signal to search engines and others "Use this URL for this page!" 
+		Useful when parameters after a # or ? is used to control the display state of a page.
+	-->
+	<!-- <link rel="canonical" href=""> -->
+
+	<!-- Mobile viewport optimized: j.mp/bplateviewport -->
+	<meta name="viewport" content="width=device-width, initial-scale=1"> 
+
 	<link rel="profile" href="http://gmpg.org/xfn/11" />
-	<!-- <link rel="stylesheet" href="<?php bloginfo( 'stylesheet_url' ); ?>" /> -->
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+	
+	<link rel="stylesheet" href="<?php bloginfo( 'stylesheet_directory' ); ?>/assets/css/reset.css" />
+	<link rel="stylesheet" href="<?php bloginfo( 'stylesheet_directory' ); ?>/assets/css/core.css" />
 
 	<?php
 		/* We add some JavaScript to pages with the comment form
 		 * to support sites with threaded comments (when in use).
 		 */
 		if ( is_singular() && get_option( 'thread_comments' ) ) {
-			wp_enqueue_script( 'comment-reply' );
+			//wp_enqueue_script( 'comment-reply' );
 		}
 
-		/* Always have wp_head() just before the closing </head>
-		 * tag of your theme, or you will break many plugins, which
-		 * generally use this hook to add elements to <head> such
-		 * as styles, scripts, and meta tags.
-		 */
 		wp_head();
 	?>
 </head>
